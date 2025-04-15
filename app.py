6da4484808e017b6eef81f6438c4b8993037629c
kchristensen17 <kchristensen17@gwu.edu>
2025-04-15 19:15:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     img = image.resize((224, 224))
     img_array = np.array(img)
